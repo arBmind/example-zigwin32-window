5c2ebac671ae856472840987c88b16287e216a1c
Andreas Reischuck <andreas.reischuck@hicknhack-software.com>
2023-12-17 16:47:32 +0100

    always use windows as target

--- build.zig
+++ build.zig
@@ -1,8 +1,7 @@
 const std = @import("std");
 
 pub fn build(b: *std.Build) void {
-    const target = b.standardTargetOptions(.{});
-
+    const target = std.zig.CrossTarget{ .os_tag = .windows, .cpu_arch = .x86_64 };
     const optimize = b.standardOptimizeOption(.{});
 
     const exe = b.addExecutable(.{
