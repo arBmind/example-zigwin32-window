896299005df0e19293029cb7205bee32b5155f36
Andreas Reischuck <andreas.reischuck@hicknhack-software.com>
2023-12-17 17:46:10 +0100

    modernize example
    * use wWinMain, also no need for callConv here
    * use _T to generate proper strings
    * use orelse to avoid if
    * use defer to ensure destruction
    * implement missing RGB macro as function

--- src/main.zig
+++ src/main.zig
@@ -1,57 +1,80 @@
 const std = @import("std");
-const win32 = @import("zigwin32");
-const win_and_mess = win32.ui.windows_and_messaging;
+const zigwin32 = @import("zigwin32");
 
-pub fn main() !void {
-    const className = "Sample Window Class";
+const WINAPI = std.os.windows.WINAPI;
+const win32 = zigwin32.foundation;
+const wam = zigwin32.ui.windows_and_messaging;
+const gdi = zigwin32.graphics.gdi;
+const _T = zigwin32.zig._T;
+const FALSE = zigwin32.zig.FALSE;
 
-    var wndClass: win_and_mess.WNDCLASSA = std.mem.zeroes(win_and_mess.WNDCLASSA);
-    wndClass.lpszClassName = className;
+pub fn wWinMain(hInstance: win32.HINSTANCE, _: ?win32.HINSTANCE, pCmdLine: [*:0]u16, nCmdShow: u32) u8 {
+    _ = pCmdLine;
+    const className = _T("Sample Window Class");
+    const title = _T("Example Window");
 
-    const instance = win32.system.library_loader.GetModuleHandleA(null);
+    var wndClass = wam.WNDCLASSEX{
+        .cbSize = @sizeOf(wam.WNDCLASSEX),
+        .style = wam.WNDCLASS_STYLES.initFlags(.{ .VREDRAW = 1, .HREDRAW = 1 }),
+        .lpfnWndProc = WindowProc,
+        .cbClsExtra = 0,
+        .cbWndExtra = 0,
+        .hInstance = hInstance,
+        .hIcon = wam.LoadIcon(null, wam.IDI_APPLICATION),
+        .hCursor = wam.LoadCursor(null, wam.IDC_ARROW),
+        .hbrBackground = gdi.GetStockObject(gdi.WHITE_BRUSH),
+        .lpszMenuName = null,
+        .lpszClassName = className,
+        .hIconSm = null,
+    };
+    _ = wam.RegisterClassEx(&wndClass);
 
-    wndClass.hInstance = instance;
-    wndClass.lpfnWndProc = WindowProc;
+    const hwnd = wam.CreateWindowEx(
+        wam.WINDOW_EX_STYLE.initFlags(.{}),
+        className, // Class name
+        title, // Window name
+        wam.WINDOW_STYLE.initFlags(.{ .SYSMENU = 1, .THICKFRAME = 1, .TILEDWINDOW = 1 }),
+        wam.CW_USEDEFAULT,
+        wam.CW_USEDEFAULT, // initial position
+        wam.CW_USEDEFAULT,
+        wam.CW_USEDEFAULT, // initial size
+        null, // Parent
+        null, // Menu
+        hInstance,
+        null, // WM_CREATE lpParam
+    ) orelse return 1;
 
-    _ = win_and_mess.RegisterClassA(&wndClass);
+    _ = wam.ShowWindow(hwnd, @enumFromInt(nCmdShow));
 
-    const hwnd = win_and_mess.CreateWindowExA(win_and_mess.WINDOW_EX_STYLE.initFlags(.{}), className, "Example Window", win_and_mess.WINDOW_STYLE.initFlags(.{ .SYSMENU = 1, .THICKFRAME = 1, .TILEDWINDOW = 1 }), win_and_mess.CW_USEDEFAULT, win_and_mess.CW_USEDEFAULT, win_and_mess.CW_USEDEFAULT, win_and_mess.CW_USEDEFAULT, null, null, instance, null);
-
-    if (hwnd == null) {
-        return;
+    var msg = std.mem.zeroes(wam.MSG);
+    while (wam.GetMessage(&msg, hwnd, 0, 0) > FALSE) {
+        _ = wam.TranslateMessage(&msg);
+        _ = wam.DispatchMessage(&msg);
     }
-
-    _ = win_and_mess.ShowWindow(hwnd, win_and_mess.SHOW_WINDOW_CMD.SHOWNORMAL);
-
-    var msg = std.mem.zeroes(win_and_mess.MSG);
-
-    while (win_and_mess.GetMessageA(&msg, hwnd, 0, 0) > 0) {
-        _ = win_and_mess.TranslateMessage(&msg);
-        _ = win_and_mess.DispatchMessageA(&msg);
-    }
+    return @intCast(msg.wParam);
 }
 
-fn WindowProc(window: win32.foundation.HWND, msg: u32, wParam: usize, lParam: isize) callconv(.C) isize {
+fn RGB(r: u8, g: u8, b: u8) u32 {
+    return r | (@as(u16, g) << 8) | (@as(u32, b) << 16);
+}
+
+fn WindowProc(window: win32.HWND, msg: u32, wParam: win32.WPARAM, lParam: win32.LPARAM) callconv(WINAPI) win32.LRESULT {
     switch (msg) {
-        win_and_mess.WM_DESTROY => {
-            win_and_mess.PostQuitMessage(0);
+        wam.WM_DESTROY => {
+            wam.PostQuitMessage(0);
             return 0;
         },
-        win_and_mess.WM_PAINT => {
-            var ps = std.mem.zeroes(win32.graphics.gdi.PAINTSTRUCT);
-            var hdc = win32.graphics.gdi.BeginPaint(window, &ps);
+        wam.WM_PAINT => {
+            var ps = std.mem.zeroes(gdi.PAINTSTRUCT);
+            const hdc = gdi.BeginPaint(window, &ps);
+            defer _ = gdi.EndPaint(window, &ps);
 
-            var color = extern struct { r: u8 = 255, g: u8 = 115, b: u8 = 55, a: u8 = 0 }{};
-
-            const brush = win32.graphics.gdi.CreateSolidBrush(@bitCast(color));
-
-            _ = win32.graphics.gdi.FillRect(hdc, &ps.rcPaint, @as(win32.graphics.gdi.HBRUSH, brush.?));
-
-            _ = win32.graphics.gdi.EndPaint(window, &ps);
+            const brush = gdi.CreateSolidBrush(RGB(255, 115, 55)) orelse return 0;
+            _ = gdi.FillRect(hdc, &ps.rcPaint, brush);
             return 0;
         },
         else => {},
     }
 
-    return win_and_mess.DefWindowProcA(window, msg, wParam, lParam);
+    return wam.DefWindowProc(window, msg, wParam, lParam);
 }
